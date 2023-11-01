4cb715eb4334fd05a31d100d248ce9e82b1d9bda
Group Fighter <finetoo97@gmail.com>
2023-11-01 08:56:09 +0700

    fix bug driver fs

--- src/runtime/server/middleware/session/sessionStorage.ts
+++ src/runtime/server/middleware/session/sessionStorage.ts
@@ -3,6 +3,9 @@ import { useRuntimeConfig } from '#imports'
 // @ts-ignore
 import sessionDriver from '#session-driver'
 const sessionConfig = useRuntimeConfig().session.session
-const driver = sessionDriver(sessionConfig.storageOptions.options)
+let driver = sessionDriver(sessionConfig.storageOptions.options)
+if('driver' in sessionConfig.storageOptions && sessionConfig.storageOptions.driver === 'fs'){
+  driver = sessionDriver({ ...sessionConfig.storageOptions.options })
+}
 const storage = createStorage({ driver }).mount(sessionConfig.storePrefix, driver)
 export const sessionStorage = prefixStorage(storage, sessionConfig.storePrefix)
